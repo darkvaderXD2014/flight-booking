7d933f8eb153a672edb08ec967497b5fea672d0c
darkvaderXD2014 <ian.sapelino12@gmail.com>
2020-03-09 14:07:20 +0800

    added some filez

--- login/views.py
+++ login/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'index.html', {})
+def login(request):
+    return render(request, 'login.html', {})
+
+def reg(request):
+    return render(request, 'reg.html', {})
