97999cdbf685a6699440d7231d43caa7ca53a4fc
darkvaderXD2014 <ian.sapelino12@gmail.com>
2020-03-10 19:36:20 +0800

    added settings-dash board

--- home/views.py
+++ home/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
+
+from .forms import EditProfileForm
+
 
 @login_required(login_url='/')
 def home(request):
     return render(request, "home.html", {})
+
+@login_required(login_url='/')
+def settings(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('settings')
+    else:
+        form = EditProfileForm(request.POST, instance=request.user)
+        args = {'form':form}
+        return render(request, 'settings.html', args)
